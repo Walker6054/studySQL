c0272e7ca67fd9a3e6d0ae2ff243a4d09c663a1d
Walker6054 <dmitiry.gaskov@ya.ru>
2022-02-25 01:40:49 +0800

    (студент) +drag&drop -валидация/отправка запроса на сервер

--- static/js/pages/tests/solve_test.js
+++ static/js/pages/tests/solve_test.js
@@ -1,26 +1,36 @@
 let dropzones = document.getElementsByClassName("dropzone");
 let drag_elems = document.getElementsByClassName("drag_elem");
-let rows_answers = document.getElementsByClassName("row_answers")[0];
+let rows_answers = document.getElementsByClassName("row_answers");
 
 if ((dropzones.length != 0) && (drag_elems.length != 0) && (rows_answers.length != 0)) {
     for (let i = 0; i < dropzones.length; i++) {
-        dropzones[i].addEventListener("ondragover", on_drag_over);
-        dropzones[i].addEventListener("ondrop", on_drop);
+        dropzones[i].addEventListener("dragover", on_drag_over);
+        dropzones[i].addEventListener("drop", on_drop);
     }
 
     for (let i = 0; i < rows_answers.length; i++) {
-        rows_answers[i].addEventListener("ondragover", on_drag_over);
-        rows_answers[i].addEventListener("ondrop", on_drop);
+        rows_answers[i].addEventListener("dragover", on_drag_over);
+        rows_answers[i].addEventListener("drop", on_drop);
     }
 
     for (let i = 0; i < drag_elems.length; i++) {
-        drag_elems[i].addEventListener("ondragstart", on_drag_start);
+        drag_elems[i].addEventListener("dragstart", on_drag_start);
+        drag_elems[i].addEventListener("dragend", on_drag_end);
     }
 }
 
 function on_drag_start() {
     event.dataTransfer.setData('text/plain', event.target.id);
+    event.target.setAttribute("class", "drag_elem selected")
 }
+function on_drag_end() {
+    if (event.target.parentElement.getAttribute("class") == "dropzone") {
+        event.target.setAttribute("class", "drag_elem_in_place");
+    } else {
+        event.target.setAttribute("class", "drag_elem");
+    }
+}
+
 function on_drag_over() {
     event.preventDefault();
 }
@@ -28,28 +38,29 @@ function on_drop() {
     let id = event.dataTransfer.getData('text');
     let draggableElement = document.getElementById(id);
     let dropzone = event.target;
-    dropzone.appendChild(draggableElement);
 
-    event.dataTransfer.clearData();   
-}
-
-
-
-function onDragStart() {
-    event.dataTransfer.setData('text/plain', event.target.id);
-}
-
-function onDragOver() {
-    event.preventDefault();
-}
-
-function onDrop() {
-    let id = event.dataTransfer.getData('text');
-    let draggableElement = document.getElementById(id);
-    if (dropzone.getAttribute("class") == "drag_elem") {
-        dropzone = dropzone.parentElement;
+    console.log(dropzone.getAttribute("class"));
+    if ((!dropzone.firstChild) || (dropzone.getAttribute("class") == "row_answers")) {
+        dropzone.appendChild(draggableElement);
     }
-    dropzone.appendChild(draggableElement);
-
     event.dataTransfer.clearData();   
 }
+
+
+let button_back_tests = document.getElementById("button_back_tests");
+button_back_tests.addEventListener("click", back_to_tests);
+function back_to_tests() {
+    let exit = confirm("Выход может привести к потере введенных данных!");
+    if (exit) {
+        window.location = window.location.origin + "/tests";
+    }
+}
+
+let button_send_to_check = document.getElementById("button_send_to_check");
+button_send_to_check.addEventListener("click", validation);
+
+function validation() {
+}
+function procces_send() {
+}
+
